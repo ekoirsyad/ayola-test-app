ef85f4579057b21d89b0924a410d16577f53cada
Eko Nur Muhammad Irsyad <eko.ir@students.amikom.ac.id>
2023-12-19 14:44:33 +0700

    initialization for tdd

---
+++ src/components/Register/Register.test.tsx
@@ -0,0 +1,9 @@
+describe('RegisterComponent', () => {
+  it.todo('Password must contain at least 8 characters');
+
+  it.todo('Password must contain at least a lowercase letter');
+
+  it.todo('Password must contain at least an uppercase letter');
+
+  it.todo('Email must meet email format validation');
+});
